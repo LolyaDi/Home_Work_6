9532dd2114e93a71e5e98ce02feb2e9625e429bf
LolyaDi <you@example.com>
2019-03-23 01:23:01 +0600

    Шестая домашняя работа

---
+++ Home_Work_6/Array.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Home_Work_6
+{
+    public class Array
+    { 
+        private const int TWO = 2;
+        private int[] _array;
+
+        public int Size { get; set; }
+
+        public Array(int size)
+        {
+            Size = size;
+            _array = new int[Size];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return _array[index];
+            }
+
+            set
+            {
+                _array[index] = Convert.ToInt32(Math.Pow(value, TWO));
+            }
+        }
+    }
+}
---
+++ Home_Work_6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace Home_Work_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NULL_ELEMENT = 0;
+            const int FIRST_ELEMENT = 1;
+            const int SECOND_ELEMENT = 2;
+            const int THIRD_ELEMENT = 3;
+            const int FOURTH_ELEMENT = 4;
+            const int FIVE = 5;
+
+            Random random = new Random();
+            Array array = new Array(FIVE);
+
+            array[NULL_ELEMENT] = random.Next(FIVE);
+            array[FIRST_ELEMENT] = random.Next(FIVE); ;
+            array[SECOND_ELEMENT] = random.Next(FIVE); ;
+            array[THIRD_ELEMENT] = random.Next(FIVE); ;
+            array[FOURTH_ELEMENT] = random.Next(FIVE); ;
+
+
+            WriteLine("Массив после возведения в квадрат: ");
+            for (int i = 0; i < array.Size; i++)
+            {
+                Write($"{array[i]}, ");
+            }
+
+            ReadKey();
+        }
+    }
+}
